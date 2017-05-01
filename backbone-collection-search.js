937c08423b235d866020a26e43bd367c597c3150
Sergey Margaritov <axeriano@gmail.com>
2017-05-01 17:33:27 +0300

    back to simple search with native String function indexOf

--- backbone-collection-search.js
+++ backbone-collection-search.js
@@ -6,11 +6,11 @@
 ;(function (root, factory) {
 
   if (typeof define === 'function' && define.amd) {
-    define(['underscore', 'backbone', 'fuse.js'], factory);
+    define(['underscore', 'backbone'], factory);
   } else if (typeof exports === 'object') {
-    module.exports = factory(require('underscore'), require('backbone'), require('fuse.js'));
+    module.exports = factory(require('underscore'), require('backbone'));
   } else {
-    factory(root._, root.Backbone, root.Fuse);
+    factory(root._, root.Backbone);
   }
 
 }(this, function (_, Backbone, Fuse) {
@@ -18,14 +18,12 @@
   // Extending out
   _.extend(Backbone.Collection.prototype, {
 
-    buildFuse: function () {
-      this.fuse = new Fuse([], {
-        keys: _.result(this, 'searchable', []),
-        id: 'cid',
-        threshold: 0.4,
-        distance: 20,
-        tokenize: true
-      });
+    //@ Default Matcher - may be overwritten
+    matcher: function(needle, haystack) {
+      if (!needle || !haystack) return;
+      needle = needle.toString().toLowerCase();
+      haystack = haystack.toString().toLowerCase();
+      return haystack.indexOf( needle ) >= 0;
     },
 
     //@ Search function
@@ -33,22 +31,22 @@
 
       // If collection empty get out
       if (!this.models.length) return;
-      if (!this.fuse) this.buildFuse();
 
-      // set collection for Fuse
-      this.fuse.set(this.map(function (model) {
-        return _.extend({ cid: model.cid }, model.attributes);
-      }));
+      var keys = _.result(this, 'searchable', []);
 
-      var results = this.fuse.search(keyword);
-
-      results = _.map(results, function (cid) { return this.get(cid); }, this);
+      // Filter
+      var matcher = this.matcher;
+      var results = this.filter(function( model ) {
+        return !_.every( keys, function( attribute ) {
+          return !matcher( keyword, model.get( attribute ) );
+        });
+      });
 
       // Instantiate new Collection
       var collection = new Backbone.Collection( results );
       collection.searching = {
         keyword: keyword,
-        attributes: _.result(this, 'searchable', [])
+        attributes: keys
       };
       collection.getSearchQuery = function() {
         return this.searching;
@@ -62,7 +60,7 @@
       var t = setTimeout(function(){
         that.trigger('search', collection);
         clearTimeout(t);
-      },10);
+      }, 10);
 
       // For use of returning un-async
       return collection;
